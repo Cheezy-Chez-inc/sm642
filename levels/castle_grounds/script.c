ef70c89d9df2dddffde74352052728575d02ce26
The1CheesyNacho <oliveirawillian72@gmail.com>
2024-02-24 15:14:08 -0300

    Revert "Update script.c"
    This reverts commit 8b04ecee6cfba914a730abe3680a5525a9eb9996.

--- levels/castle_grounds/script.c
+++ levels/castle_grounds/script.c
@@ -104,7 +104,7 @@ const LevelScript level_castle_grounds_entry[] = {
     LOAD_MIO0(        /*seg*/ 0x08, _common0_mio0SegmentRomStart, _common0_mio0SegmentRomEnd),
     LOAD_RAW(         /*seg*/ 0x0F, _common0_geoSegmentRomStart,  _common0_geoSegmentRomEnd),
     ALLOC_LEVEL_POOL(),
-    MARIO(/*model*/ MODEL_MARIO, /*behParam*/ 0x00000000, /*beh*/ bhvMario),
+    MARIO(/*model*/ MODEL_MARIO, /*behParam*/ 0x00000001, /*beh*/ bhvMario),
     LUIGI(/*model*/ MODEL_LUIGI, /*behParam*/ 0x00000001, /*beh*/ bhvLuigi),
     JUMP_LINK(script_func_global_1),
     JUMP_LINK(script_func_global_11),
